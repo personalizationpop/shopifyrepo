65e74f08c04712b176d0c2da7d38dedaa014b036
adeelabid <adeelabid>
2016-07-25 11:17:11 +0500

    install

--- routes/shopify.js
+++ routes/shopify.js
@@ -39,8 +39,8 @@ shopifyRouter.get('/finish_auth',function (req,res,next) {
 
     //var Shopify = new shopifyAPI(config), // You need to pass in your config here
     var query_params = req.query;
-
-    Shopify.exchange_temporary_token(query_params, function(err, data){
+    console.log(query_params);
+    /*Shopify.exchange_temporary_token(query_params, function(err, data){
 
         if (!self.is_valid_signature(query_params,true)) {
             return callback(new Error("Signature is not authentic!"));
@@ -50,7 +50,7 @@ shopifyRouter.get('/finish_auth',function (req,res,next) {
         // Otherwise err will be non-null.
         // The module will automatically update your config with the new access token
         console.log(data['access_token']);
-    });
+    });*/
 
 });
 
