83955e88809874c8f577479cbc29ee2c6ffb1769
adeelabid <adeelabid>
2016-07-25 14:53:11 +0500

    install

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// models
+app.set('models', path.join(__dirname, 'models'));
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
