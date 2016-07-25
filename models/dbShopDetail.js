7d325a26bd0ee1fda7bdfb8a94b5675168bc5aa9
adeelabid <adeelabid>
2016-07-25 14:49:50 +0500

    install

--- models/dbShopDetail.js
+++ models/dbShopDetail.js
@@ -2,22 +2,22 @@
  * Created by Admin on 7/25/2016.
  */
 
-var mongoose = require('mongoose');
+/*var mongoose = require('mongoose');
 mongoose.connect('mongodb://adeel:admin123@ds029735.mlab.com:29735/dbtestapp');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+
     // we're connected!
-    var shopSchema = new Schema({
-        restrict:false
-    });
-
-    var clcShopDetail = mongoose.model('clcShopDetail', shopSchema);
-
-    // make this available to our users in our Node applications
-    module.exports = clcShopDetail;
-
+var shopSchema = new Schema({
+    restrict:false
 });
+
+var clcShopDetail = mongoose.model('clcShopDetail', shopSchema);
+
+// make this available to our users in our Node applications
+module.exports = clcShopDetail;
+
+*/
+
+
 
 
