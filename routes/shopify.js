89f77c9ece909a07f120e85326b948cb33f71fcf
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-26 15:14:34 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -53,7 +53,7 @@ dbCollectionShopDetail.find({shop:shop},function(err, result) {
        
        
         var resJson = JSON.stringify(result,undefined,2);
-        var resParse = JSON.parse(resJson.toString());
+        var resParse = JSON.parse(resJson.toString().trim());
         console.log("resParse[0].shop : "+ resParse[0].shop);
         res.send(resJson);
    
