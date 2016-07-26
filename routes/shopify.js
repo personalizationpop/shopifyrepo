ea0bb008df4a74f1ab8a6e8661644d8ab840a755
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-26 14:04:17 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -53,14 +53,14 @@ dbCollectionShopDetail.find({shop:shop},function(err, result) {
        //res.send(JSON.stringify(result, undefined, 2)); print json
        //var resJson2 = JSON.parse(result);
        //var resJson = JSON.stringify(result, undefined, 2);
-       var resParse = JSON.parse(result);
+       //var resParse = JSON.parse(result);
        var resString = JSON.stringify(result, undefined, 2);
        console.log('result : '+ result);
        console.log("result['shop'] : "+ result['shop']);
        console.log("result[0]['shop'] : "+ result[0]['shop']);
-              console.log("resParse : "+ resParse);
-       console.log("resParse['shop'] : "+ resParse['shop']);
-       console.log("resParse[0]['shop'] : "+ resParse[0]['shop']);
+              //console.log("resParse : "+ resParse);
+       //console.log("resParse['shop'] : "+ resParse['shop']);
+      // console.log("resParse[0]['shop'] : "+ resParse[0]['shop']);
         console.log('resString : '+ resString);
        console.log("resString['shop'] : "+ resString['shop']);
        console.log("resString[0]['shop'] : "+ resString[0]['shop']);
