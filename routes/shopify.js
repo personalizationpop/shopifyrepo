e36df3a721821873b9686a459531b07b677d2dc3
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-26 12:31:12 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -49,6 +49,7 @@ dbCollectionShopDetail.find({shop:shop},function(err, result) {
       console.log(err+ ' error');
     } else {
        console.log('result '+ result.length);
+       res.send(result.shop);
     }
     });  
 
