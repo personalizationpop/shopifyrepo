9d662e3c85c6d0ed4c7098aa851da852d1849b65
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-26 17:12:30 +0500

    Update recurring.js

--- routes/recurring.js
+++ routes/recurring.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(request.body.plan1);
+  console.log(req.body.plan1);
   res.send('i am in recurring');
 });
 
