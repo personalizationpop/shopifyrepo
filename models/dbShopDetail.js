bfb6e10be45415dcc44c845e9bda1f49edb06906
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-26 12:26:48 +0500

    Update dbShopDetail.js

--- models/dbShopDetail.js
+++ models/dbShopDetail.js
@@ -9,9 +9,9 @@ var mongoose = require('mongoose');
     var shopSchema = new mongoose.Schema({},{
         strict:false
     });
-    
+    shopSchema.set('collection', 'clcShopDetail'); // only added to prevent from automatically making plural etc..clcShopDetails because mongoose auto make plural if not
 
-    var clcShopDetail = mongoose.model('clcShopDetails', shopSchema);
+    var clcShopDetail = mongoose.model('clcShopDetail', shopSchema);
 
 // make this available to our users in our Node applications
     module.exports = clcShopDetail;
