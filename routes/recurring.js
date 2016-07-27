ec704904f979ab84551a8b6ced1e479bd99c740a
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-27 08:39:34 +0500

    Update recurring.js

--- routes/recurring.js
+++ routes/recurring.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log('req: '+ req.body.plan2);
+  console.log('req: '+ req.body.plan1);
   res.send('Form Submitted');
 });
 
