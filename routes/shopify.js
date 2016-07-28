5083e6c415bb90fe5dc7be227e09655924ad518f
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-28 11:11:17 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -94,7 +94,7 @@ shopifyRouter.post('/createRecurringCharge',function(req, res, next){
             Shopify.post('/admin/recurring_application_charges.json',postData,function(err,result,header){
                 
                 /////Insert response into db //////////
-                /*var recurringCharge = new dbRecurringChargeDetail(result);
+                var recurringCharge = new dbRecurringChargeDetail(result);
 
                 //save model to MongoDB
                 recurringCharge.save(function (err) {
@@ -102,10 +102,9 @@ shopifyRouter.post('/createRecurringCharge',function(req, res, next){
                 		return err;
                   }
                   else {
-                  	console.log("Post saved");
+                  	console.log("New Recurring Charge Saved");
                   }
-                });*/
-                console.log('recuringChargeId :' + result.id);
+                });
                 console.log("result['recurring_application_charge'].id :" + result['recurring_application_charge'].id);
                 res.send(JSON.stringify(result,undefined,2));
                 //res.redirect('./getProducts');
