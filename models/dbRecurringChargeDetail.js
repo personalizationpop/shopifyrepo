496b7f1fc9ed1eadf6c61e39998016005d8032c4
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-28 15:36:19 +0500

    Update dbRecurringChargeDetail.js

--- models/dbRecurringChargeDetail.js
+++ models/dbRecurringChargeDetail.js
@@ -5,9 +5,9 @@ var mongoose = require('mongoose');
     var recurringChargeSchema = new mongoose.Schema({},{
         strict:false
     });
-    recurringChargeSchema.set('collection', 'clcRecurringChargeDetail'); // only added to prevent from automatically making plural etc..clcShopDetails because mongoose auto make plural if not
+    recurringChargeSchema.set('collection', 'clcShopRecurringCharge'); // only added to prevent from automatically making plural etc..clcShopDetails because mongoose auto make plural if not
 
-    var clcRecurringChargeDetail = mongoose.model('clcRecurringChargeDetail', recurringChargeSchema);
+    var clcShopRecurringCharge = mongoose.model('clcShopRecurringCharge', recurringChargeSchema);
 
 // make this available to our users in our Node applications
-    module.exports = clcRecurringChargeDetail;
+    module.exports = clcShopRecurringCharge;
