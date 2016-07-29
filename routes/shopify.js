0d5629971e2d349236a6c4e308e096bfe50dd0b8
adi9999 <adeel.abid.sofizar@gmail.com>
2016-07-29 11:16:21 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -116,6 +116,7 @@ shopifyRouter.get('/activateRecurringCharge',function(req, res, next){
     console.log("ChargeId :" + chargeId);
     dbShopRecurringChargeDetail.findOne({"recurring_application_charge.id": parseInt(chargeId) }, function(err,obj) {
         if(err){console.log("err while activation :"+err)}else{
+            console.log('id:'+obj.get("recurring_application_charge").id);
             if(obj.length>0)
             {
                 var recurringChargeDetail = obj.get("recurring_application_charge");
