d673773bf7b674d70ad3224bfeb3a03696e129e6
personalizationpop <ramin@personalizationpop.com>
2016-09-06 11:57:48 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -47,11 +47,16 @@ function getShopToken(shop,callback){
 
 /* Auth Process. */
 shopifyRouter.get('/', function(req, res, next) {
-    var Shopify = new shopifyAPI(shopifyRouter.config);
-    var auth_url = Shopify.buildAuthURL();
+    
+    var query_params = req.query;
+    console.log(req.query.shop);
+    var shop = query_params['shop'];
+    shopifyRouter.shop = shop;
+    //var Shopify = new shopifyAPI(shopifyRouter.config);
+    //var auth_url = Shopify.buildAuthURL();
 // Assuming you are using the express framework
 // you can redirect the user automatically like so
-    res.redirect(auth_url);
+    //res.redirect(auth_url);
 
 });
 
